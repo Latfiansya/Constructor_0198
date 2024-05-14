d010f97058884c4a9ef59f0c9ba70098eb1b353e
Latfiansya <latfiansyadzikri@gmail.com>
2024-05-14 19:25:14 +0700

    Memanggil Constructor dan Destructor pada int main()

--- Constructor-5/Constructor-5.cpp
+++ Constructor-5/Constructor-5.cpp
@@ -42,8 +42,9 @@ void angka::isiData() {
     cout << endl;
 }
 
-int main() {
-    angka belajarcpp(3); // Constructor Dipanggil
+int main() 
+{
+    angka belajarcpp(3); // Constructor Dipanggil 
     angka *ptrBelajarcpp = new angka(5); // Constructor Dipanggil
     delete ptrBelajarcpp; // Destructor Dipanggil
 
